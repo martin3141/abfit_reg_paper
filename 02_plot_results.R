3c6a55645a3afe4133ed11c1776b622d0eba64fd
Martin Wilson <martin@pipegrep.co.uk>
2024-10-17 14:47:49 +0100

    first plot done

--- 02_plot_results.R
+++ 02_plot_results.R
@@ -1,78 +1,101 @@
 library(ggplot2)
 library(ggsignif)
 library(dplyr)
+library(cowplot)
+
+res_dir <- "fitting_results"
+
+# create an output dir for the figures
+dir.create("figures", showWarnings = FALSE)
 
 true_amps <- read.csv(file.path("synth_data", "true_amps.csv"))
-fit_amps  <- read.csv(file.path("fitting_results_128",
-                                "fit_amps_snr_100_pdist_norm.csv"))
 
-# check the basis ordering hasn't been confused
-if (!identical(colnames(true_amps), colnames(fit_amps)[1:length(true_amps)])) {
-  stop("fit and true names do not match")
+SNRS <- c("10", "30", "60", "100")
+
+fit_amps <- vector("list", length(SNRS))
+
+fit_errors_plot <- vector("list", length(SNRS))
+
+for (n in 1:length(SNRS)) {
+  
+  fname <- paste0("fit_amps_snr_", SNRS[n], "_pdist_norm.csv")
+  
+  fit_amps <- read.csv(file.path(res_dir, fname))
+  
+  # confirm the basis ordering hasn't been confused
+  if (!identical(colnames(true_amps),
+                 colnames(fit_amps)[1:length(true_amps)])) {
+    stop("fit and true names do not match")
+  }
+  
+  theme_set(theme_classic())
+  
+  N_spec    <- max(fit_amps$num)
+  N_methods <- length(unique(fit_amps$method))
+  N_basis   <- ncol(true_amps)
+  
+  rm_inds <- grep("^Lip|^MM|^X.CrCH2", colnames(true_amps))
+  fit_errors <- fit_amps[, 1:N_basis] - true_amps[rep(1, N_spec * N_methods), ]
+  fit_errors_cut <- data.frame(error = rowSums(fit_errors[, -rm_inds] ^ 2))
+  fit_errors_cut['method'] <- fit_amps['method']
+  fit_errors_cut['num']    <- fit_amps['num']
+  fit_errors_plot[[n]]     <- fit_errors_cut
 }
 
-# make sure the ordering is correct
-# true_amps <- true_amps[,colnames(fit_amps)[1:29]]
+max_y <- fit_errors_plot[[1]] |> filter(method == "ABfit") |> select(error) |> 
+  unlist() |>  as.numeric() |> (\(x) quantile(x)[4] + 1.5 * IQR(x))() |> 
+  as.numeric()
 
-N_spec    <- max(fit_amps$num)
-N_methods <- length(unique(fit_amps$method))
-N_basis   <- ncol(true_amps)
+# signif_map <- function(p) sprintf("p = %.2g", p)
+signif_map <- c("*"=0.05, "**"=0.005, "***"=0.0005)
+comps <- list(c("ABfit-reg", "LCModel"), c("ABfit", "ABfit-reg"))
 
-rm_inds <- grep("^Lip|^MM|^X.CrCH2", colnames(true_amps))
-fit_errors <- fit_amps[, 1:N_basis] - true_amps[rep(1, N_spec * N_methods), ]
-fit_errors_cut <- data.frame(error = rowSums(fit_errors[, -rm_inds] ^ 2))
-fit_errors_cut['method'] <- fit_amps['method']
-fit_errors_cut['num']    <- fit_amps['num']
+plot_a <- ggplot(data = fit_errors_plot[[1]], aes(x = method, y = error)) +
+  geom_boxplot(width = 0.15, outliers = FALSE) +
+  geom_signif(comparisons = comps, test = "t.test",
+              map_signif_level = signif_map,
+              y_position = max_y, tip_length = 0.01, textsize = 3.5,
+              vjust = -0.2) +
+  xlab(NULL) + ylab(error~(mM^2)) + ylim(c(0, max_y * 1.28))
 
-ggplot(data = filter(fit_errors_cut, method != "ABfit"), aes(x = method, y = error)) +
-  geom_point(size = 2, alpha = 0.2, position = position_jitter(seed = 1, height = 0.0, width = 0.05)) +
-  geom_boxplot(width = 0.05, outlier.shape = NA, position = position_nudge(x = 0.12)) +
-  geom_signif(comparisons = list(c("ABfit-reg", "LCModel")), test = "t.test",
-              map_signif_level = function(p) sprintf("p = %.2g", p)) +
-  theme_classic() + xlab(NULL) + ylab("error")
+max_y <- fit_errors_plot[[2]] |> filter(method == "ABfit") |> select(error) |> 
+  unlist() |>  as.numeric() |> (\(x) quantile(x)[4] + 1.5 * IQR(x))() |> 
+  as.numeric()
 
-# ggplot(data = fit_errors_cut, aes(x = method, y = error)) +
-#   geom_boxplot(width = 0.10) +
-#   geom_signif(comparisons = list(c("ABfit-reg", "LCModel"), c("ABfit", "ABfit-reg")), test = "t.test",
-#               map_signif_level = function(p) sprintf("p = %.2g", p)) +
-#   theme_classic() + xlab(NULL) + ylab("error") +
-#   stat_summary(fun = mean, geom = 'point', shape = 1, size = 2, col = "red") +
-#   stat_summary(fun = mean, geom = 'line', group = 1, col = "red") 
+plot_b <- ggplot(data = fit_errors_plot[[2]], aes(x = method, y = error)) +
+  geom_boxplot(width = 0.15, outliers = FALSE) +
+  geom_signif(comparisons = comps, test = "t.test",
+              map_signif_level = signif_map,
+              y_position = max_y, tip_length = 0.015, textsize = 3.5,
+              vjust = -0.2) +
+  xlab(NULL) + ylab(error~(mM^2)) + ylim(c(0, max_y * 1.25))
 
+max_y <- fit_errors_plot[[3]] |> filter(method == "ABfit") |> select(error) |> 
+  unlist() |>  as.numeric() |> (\(x) quantile(x)[4] + 1.5 * IQR(x))() |> 
+  as.numeric()
 
-filter(fit_errors_cut, method == "ABfit-reg") |> select(error) |> colMeans()
-filter(fit_errors_cut, method == "LCModel")   |> select(error) |> colMeans()
+plot_c <- ggplot(data = fit_errors_plot[[3]], aes(x = method, y = error)) +
+  geom_boxplot(width = 0.15, outliers = FALSE) +
+  geom_signif(comparisons = comps, test = "t.test",
+              map_signif_level = signif_map,
+              y_position = max_y, tip_length = 0.008, textsize = 3.5,
+              vjust = -0.2) +
+  xlab(NULL) + ylab(error~(mM^2)) + ylim(c(0, max_y * 1.35))
 
-filter(fit_errors_cut, method == "ABfit-reg") |> select(error) |> unlist() |> which.max()
+max_y <- fit_errors_plot[[4]] |> filter(method == "ABfit") |> select(error) |> 
+  unlist() |>  as.numeric() |> (\(x) quantile(x)[4] + 1.5 * IQR(x))() |> 
+  as.numeric()
 
-# fits <- readRDS(file.path("fitting_results", "fit_res_snr_100_pdist_norm.rds"))
-# fits[[2]] |> plot(n = 471, plot_sigs = "Asp")
-# fits[[3]] |> plot(n = 471, plot_sigs = "Asp")
+plot_d <- ggplot(data = fit_errors_plot[[4]], aes(x = method, y = error)) +
+  geom_boxplot(width = 0.15, outliers = FALSE) +
+  geom_signif(comparisons = comps, test = "t.test",
+              map_signif_level = signif_map,
+              y_position = max_y, tip_length = 0.006, textsize = 3.5,
+              vjust = -0.2) +
+  xlab(NULL) + ylab(error~(mM^2)) + ylim(c(0, max_y * 1.45))
 
-#fits[[2]] |> plot(n = 862, plot_sigs = "Asp")
-#fits[[3]] |> plot(n = 862, plot_sigs = "Asp")
+labs <- paste0(c("SNR = "), c(10, 30, 60, 100))
+plot_grid(plot_a, plot_b, plot_c, plot_d, labels = labs, label_size = 11, 
+          scale = 0.95, label_x = 0.38, align = "v")
 
-# ggplot(data = fit_errors_cut, aes(x = method, y = error)) +
-#   geom_point(size = 2, alpha = 0.2, position = position_jitter(seed = 1, height = 0.0, width = 0.05)) +
-#   geom_boxplot(width = 0.05, outlier.shape = NA, position = position_nudge(x = 0.12)) +
-#   geom_signif(comparisons = list(c("ABfit-reg", "LCModel"), c("ABfit-reg", "ABfit"), 
-#                                  c("ABfit", "LCModel")), test = "t.test",
-#               map_signif_level = function(p) sprintf("p = %.2g", p), y_position = c(15, 17, 19)) +
-#   theme_classic() + xlab(NULL) + ylab("error")
-
-# fit_errors_tcr <- fit_errors[, -rm_inds]
-# fit_errors_tcr['tCr'] <- fit_errors['Cr'] + fit_errors['PCr']
-# fit_errors_tcr['Cr']  <- NULL
-# fit_errors_tcr['PCr'] <- NULL
-# fit_errors_tcr <- data.frame(error = rowSums(fit_errors_tcr ^ 2))
-# fit_errors_tcr['method'] <- fit_amps['method']
-# fit_errors_tcr['num']    <- fit_amps['num']
-
-# ggplot(data = fit_errors_tcr, aes(x = method, y = error)) +
-#   geom_point(size = 2, alpha = 0.2, position = position_jitter(seed = 1, height = 0.0, width = 0.05)) +
-#   geom_boxplot(width = 0.05, outlier.shape = NA, position = position_nudge(x = 0.12)) +
-#   geom_signif(comparisons = list(c("ABfit-reg", "LCModel"), c("ABfit-reg", "ABfit"), 
-#                                  c("ABfit", "LCModel")), test = "t.test",
-#               map_signif_level = function(p) sprintf("p = %.2g", p), y_position = c(15, 17, 19)) +
-#   theme_classic() + xlab(NULL) + ylab("error")
-
+ggsave(file.path("figures", "fit_res_norm.pdf"), scale = 0.8)
